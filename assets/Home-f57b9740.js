96333ac576332e5d4a5c21ae9eb251217ebe78ee
laflame102 <laflame4308@gmail.com>
2023-04-26 23:32:40 +0300

    Updates

---
+++ assets/Home-f57b9740.js
@@ -0,0 +1,5 @@
+import{s as t,j as e}from"./index-0887f650.js";const s=t.h1`
+  display: flex;
+  justify-content: center;
+  font-size: 60px;
+`,n=()=>e.jsx("div",{children:e.jsx(s,{children:"Welcome to tweets"})});export{n as default};
